5ff9a920a587ac953a2705b72aad4ef306dc28c2
chubbyyb <keithsalhani7@gmail.com>
2024-03-27 02:35:27 +0000

    Added regex matching

--- src/main/java/com/doculyzer/app.java
+++ src/main/java/com/doculyzer/app.java
@@ -4,17 +4,17 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class app {
-    public HashMap<String, Integer> checkWordInFile(String word, String[] selectedFiles)
-    {
+    public HashMap<String, Integer> checkWordInFile(String wordPattern, String[] selectedFiles) {
         // make a dictionary of every file and how many times the word appears in it
         HashMap<String, Integer> fileOccurrences = new HashMap<>();
 
-        for (String file : selectedFiles) {
-            System.out.println(file);
-        }
-        
+        // Compile the regular expression pattern
+        Pattern pattern = Pattern.compile(wordPattern);
+
         // for every selected file
         for (String file : selectedFiles) {
             try {
@@ -22,8 +22,10 @@ public class app {
                 BufferedReader reader = new BufferedReader(new FileReader(file));
                 String line;
                 while ((line = reader.readLine()) != null) {
+                    // Create matcher for line
+                    Matcher matcher = pattern.matcher(line);
                     // Check if the line contains the word
-                    if (line.contains(word)) {
+                    while (matcher.find()) {
                         System.out.println("Found in " + file + ": " + line);
                         // If the file is already in the dictionary, increment the count
                         if (fileOccurrences.containsKey(file)) {
@@ -31,15 +33,15 @@ public class app {
                         } else {
                             // Otherwise, add the file to the dictionary
                             fileOccurrences.put(file, 1);
+                        }
                     }
-                }
                 }
                 reader.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        return fileOccurrences;     
+        return fileOccurrences;
     }
     
     public static void main(String[] args) {
